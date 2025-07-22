d84f8c44f11f97ef61a5eac4dec95a014548195d
noufalkakdlr <noufalkakdlr@gmail.com>
2025-07-22 20:59:16 +0530

    fix typo

--- Frontend/src/pages/ShowBook.jsx
+++ Frontend/src/pages/ShowBook.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import BackButton from "../components/backButton";
+import BackButton from "../components/BackButton";
 import DeleteModal from "../components/DeleteModal";
 
 const ShowBook = () => {
